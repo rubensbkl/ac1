9a8fcb8aaafec26292c042d96e783465daedb8ca
Rubens Dias <rubens.dias08@hotmail.com>
2025-02-09 15:36:01 -0300

    tearw

--- guias/g01 Guia 01/Guia_0101.v
+++ guias/g01 Guia 01/Guia_0101.v
@@ -1,22 +1,48 @@
-/*
- Guia_0101.v
- 999999 - Xxx Yyy Zzz
-*/
+/**
+ *
+ * Pontificia Universidade Catolica de Minas Gerais
+ *
+ * Curso de Ciencia da Computacao
+ * Arquitetura de Computadores I
+ *
+ * Guia 01 - v1.0 - 08 / 02 / 2025
+ * 855796 - Rubens Dias Bicalho
+ *
+ */
 
 module Guia_0101;
+    // Definir inteiros
+    integer x1 = 27;
+    integer x2 = 51;
+    integer x3 = 725;
+    integer x4 = 132;
+    integer x5 = 360;
 
-// define data
-    integer x = 13; // decimal
-    reg [7:0] b = 0; // binary (bits - little endian)
+    // Definir registradores
+    reg [7:0] b1 = 0;
+    reg [7:0] b2 = 0;
+    reg [9:0] b3 = 0;
+    reg [7:0] b4 = 0;
+    reg [8:0] b5 = 0;
 
-// actions
+    // actions
     initial
-        begin : main
-            $display ( "Guia_0101 - Tests" );
-            $display ( "x = %d" , x );
-            $display ( "b = %8b", b );
-            b = x;
-            $display ( "b = %8b", b );
-        end // main
+    begin : main
+        // Identificação
+        $display("Guia_0101 - Conversão de Decimal para Binário");
 
-endmodule // Guia_0101
+        // Atribuir e converter valores
+        b1 = x1;
+        b2 = x2;
+        b3 = x3;
+        b4 = x4;
+        b5 = x5;
+        
+        // Mostrar resultados
+        $display("x1 = %d | b1 = %10b", x1, b1);
+        $display("x2 = %d | b2 = %10b", x2, b2);
+        $display("x3 = %d | b3 = %10b", x3, b3);
+        $display("x4 = %d | b4 = %10b", x4, b4);
+        $display("x5 = %d | b5 = %10b", x5, b5);
+    end
+endmodule
